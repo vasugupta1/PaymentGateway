2b2eaafaa366df6ce3350c779418f6a3b9122f79
Vasu Gupta <vasugupta1@gmail.com>
2021-04-22 08:25:04 +0100

    Update UserService.cs

--- src/PaymentGateway.Services/AuthUsers/UserService.cs
+++ src/PaymentGateway.Services/AuthUsers/UserService.cs
@@ -13,8 +13,11 @@ namespace PaymentGateway.Services.AuthUsers
             ?? throw new ArgumentNullException(nameof(usernamesAndPasswordDatabase));
         }
         ///
-        // If this code written for production, then we will have a seperate service which will communication with a external database that will contain a list of usersnames and passwords, but for the sake of this project
-        // a dictonary is used to keep track of username and passwords which are allowed to use the two endpoints. This dictinary of username and passwords can be found in appsettings.json file under the CustomConfiguration:Authentication property
+        // If this code written for production, then we will have a seperate service which       
+        //will communicate with a external database that will contain a list of usersnames
+        //and passwords, but for the sake of this project a dictonary is used to keep track of 
+        //username and passwords which are allowed to use the two endpoints. This dictinary of username 
+        //and passwords can be found in appsettings.json file under the CustomConfiguration:Authentication property
         ///
         public bool IsValid(string username, string password)
         {
